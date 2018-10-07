64dac9c15d84ac59be265649de10067bd6d62581
terzoc <doctort1231@gmail.com>
2018-10-07 11:15:14 -0400

    trying to add drill download

--- script/index.js
+++ script/index.js
@@ -1,4 +1,12 @@
 function test(){
   var instrument = document.getElementById("instrument").value;
 console.log(instrument);
+f
+}
+
+function download() {
+    $('#link').attr({
+      target: '_blank',
+      href : "http://localhost:drill/a1.pdf"
+    })
 }
