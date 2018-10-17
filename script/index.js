a3e50efa22baf2d615513f4a801edb15d00bcfe0
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-10-17 14:27:11 -0400

    drill download js

--- script/index.js
+++ script/index.js
@@ -1,12 +1,27 @@
 function test(){
   var instrument = document.getElementById("instrument").value;
-console.log(instrument);
-f  // <-- what's this? -kyle
+  var number = document.getElementById("number").value;
+  if (number = "") {
+    alert("Please enter a Drill Number");
+  }else {
+    var drillNum = instrument+number;
+    console.log(drillNum);
+    download(drillNum);
+  }
+
+
 }
 
-function download() {
-    $('#link').attr({
-      target: '_blank',
-      href : "http://localhost:drill/a1.pdf"
-    })
+function download(drillNum) {
+    switch (drillNum) {
+      case "T2":
+        console.log("downlad T2")
+        break;
+      default:
+
+    }
+    // $('#link').attr({
+    //   target: '_blank',
+    //   href : "http://localhost:drill/a1.pdf"
+    // })
 }
