622debae2fd0cc090d701059b6f101611a88bcad
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-12-04 14:48:41 -0500

    more drill

--- script/index.js
+++ script/index.js
@@ -18,17 +18,62 @@ function test(){
 
 function download(drillNum) {
     switch (drillNum) {
-      case "A1":
-        console.log("download A1");
-        window.open('drill/A1.pdf', '_blank');
-        break;
-      case "C1":
-          window.open('drill/C1.pdf', '_blank');
-          break;
-      case "T2":
-        console.log("downlad T2");
-        window.open('drill/T2.pdf', '_blank');
-        break;
+        case "A1":
+            console.log("download A1");
+            window.open('drill/A1.pdf', '_blank');
+            break;
+        case "B1":
+            window.open('drill/B1.pdf', '_blank');
+            break;
+        case "B2":
+            window.open('drill/B2.pdf', '_blank');
+            break;
+        case "B3":
+            window.open('drill/B3.pdf', '_blank');
+              break;
+        case "B4":
+              window.open('drill/CB.pdf', '_blank');
+              break;
+        case "B5":
+              window.open('drill/CB.pdf', '_blank');
+              break;
+        case "B6":
+             window.open('drill/B6.pdf', '_blank');
+                break;
+              case "B7":
+                window.open('drill/B7.pdf', '_blank');
+                break;
+              case "B8":
+                window.open('drill/B8.pdf', '_blank');
+                break;
+          case "C1":
+              window.open('drill/C1.pdf', '_blank');
+              break;
+          case "C2":
+              window.open('drill/C2.pdf', '_blank');
+              break;
+          case "C3":
+              window.open('drill/C3.pdf', '_blank');
+              break;
+          case "C4":
+              window.open('drill/C4.pdf', '_blank');
+              break;
+          case "C5":
+              window.open('drill/C5.pdf', '_blank');
+            break;
+          case "C6":
+            window.open('drill/C6.pdf', '_blank');
+            break;
+          case "C7":
+            window.open('drill/C7.pdf', '_blank');
+            break;
+          case "C8":
+            window.open('drill/C8.pdf', '_blank');
+            break;
+          case "T2":
+              console.log("downlad T2");
+              window.open('drill/T2.pdf', '_blank');
+              break;
       default:
 
     }
